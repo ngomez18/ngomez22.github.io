3f5549ac6b0a88b7355ba13099d9fe7e58279954
Nicolás Gómez <n.gomez22@uniandes.edu.co>
2017-05-02 08:41:26 -0500

    Copy previous website from older repo

---
+++ js/sketch.js
@@ -0,0 +1,41 @@
+(function setup() {
+  //Setup sidebar links
+  var sidebarLinks = document.getElementsByClassName("sidebar-link");
+  for (var i = 0; i < sidebarLinks.length; i++) {
+    var href = sidebarLinks[i].getAttribute("href").replace(/#/g, "");
+    var target = document.getElementById(href);
+    sidebarLinks[i].addEventListener("click", function(event) {
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+    })
+  }
+
+  //Setup profile pic beahviour
+  var pic = document.getElementById("ppic");
+  var picWidth = pic.getAttribute("width");
+  pic.addEventListener("mouseover", function(event) {
+    pic.src = "./files/uniandes.jpg";
+    pic.classList.add("outline");
+  });
+  pic.addEventListener("mouseleave", function(event) {
+    pic.src = "./files/profilepic.png";
+    pic.classList.remove("outline");
+  })
+
+  //Setup social media buttons highlight functionality
+  var buttons = document.getElementsByClassName("social-media");
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("mouseover", function(event) {
+      event.srcElement.classList.add("highlight");
+    });
+    buttons[i].addEventListener("mouseleave", function(event) {
+      event.srcElement.classList.remove("highlight");
+    })
+  }
+
+  //Setup send button
+  document.getElementById("send-btn").addEventListener("click", function() {
+    console.log("Algún día se enviará el correo...");
+  });
+})()
